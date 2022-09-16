cfc2ec9fe77fef1c0d91075c5d2fc00fc656f447
Zayne Arnold <Zayne.a@gmail.com>
2022-09-16 09:36:04 +0200

    init

---
+++ README.md
@@ -0,0 +1,144 @@
+# Dropbox API Demo
+
+A Python script that connects to and uses the Dropbox V2 API
+
+Info:
+
+The app connects to Dropbox via an access token, which lasts for 4 hours. 
+We built in a caching mechanism that stores this access token to disk for three hours, as we would then otherwise be creating a new 4 hour access token on each run of the script.
+
+If the script is run again any time after 3 hours, a new token will be retrieved, and cached.
+
+The program itself has access to the account holder's personal info (we greet the user by name).
+
+It also pulls the remote file list, as well as metadata (only modified date for now)
+
+Storage info is retrieved as well (total space allocation as well as space used.)
+
+A somewhat randomly named file is then generated, populated with the current date stamp, and then uploaded to Dropbox.
+
+Once this is done, the update remote file list is then retrieved.
+
+
+# Setup - Installing Python (Linux):
+To see which version of Python 3 you have installed, open a command prompt and run
+
+```
+$ python3 --version
+```
+
+If you are using Ubuntu 16.10 or newer, then you can easily install Python 3.6 with the following commands:
+
+```
+$ sudo apt-get update
+$ sudo apt-get install python3.6
+```
+
+If you’re using another version of Ubuntu (e.g. the latest LTS release) or you want to use a more current Python, we recommend using the deadsnakes PPA to install Python 3.8:
+
+```
+$ sudo apt-get install software-properties-common
+$ sudo add-apt-repository ppa:deadsnakes/ppa
+$ sudo apt-get update
+$ sudo apt-get install python3.8
+```
+
+At this point, you may have system Python 2.7 available as well.
+```
+$ python
+```
+This might launch the Python 2 interpreter.
+```
+$ python3
+```
+This will always launch the Python 3 interpreter.
+
+
+# Setup - Installing Python (Windows):
+
+1. Open your web browser and navigate to the Downloads for Windows section of the official Python website.
+2. Search for Python3
+3. Select a link to download either the Windows x86-64 executable installer or Windows x86 executable installer
+4. For all recent versions of Python, the recommended installation options include *Pip* and *IDLE*. Older versions might not include such additional features.
+
+# Setup - Installing Pip:
+
+To see if pip is installed, open a command prompt and run
+```
+$ command -v pip
+```
+Note that on some Linux distributions including Ubuntu and Fedora the pip command is meant for Python 2, while the pip3 command is meant for Python 3.
+```
+$ command -v pip3
+```
+*Installation*
+Try
+```
+python -m ensurepip --upgrade
+```
+and if the above fails, try the alternate method using get-pip.py: 
+
+Download the script, from [https://bootstrap.pypa.io/get-pip.py](https://bootstrap.pypa.io/get-pip.py). 
+
+Open a terminal/command prompt, cd to the folder containing the get-pip.py file and run:
+```
+python get-pip.py
+```
+
+# Setup - Installing curl:
+The program makes use of the curl program, which is usually installed on most servers. 
+
+To test if you have curl installed, simply do the following from the terminal, on all OS's
+```
+curl
+```
+
+To install it on Linux, run the following from the terminal
+```
+sudo apt-get install curl
+```
+To install curl on Windows
+1. In Windows, create a folder called curl in your C: drive.
+
+2. Go to http://curl.haxx.se/download.html and download one of the following zip files:
+
+  - If you have a Windows 64 system, scroll to the Win64 - Generic section and look for the latest Win64 ia64 zip version with SSL support. It's normally second in the list. Click the version number to start the download.
+  - If you have a Windows 32 system, scroll to the Win32 - Generic section and look for the latest Win32 zip version with SSL support. It's normally second in the list. Click the version number to start the download.
+
+3. Unzip the downloaded file and move the curl.exe file to your C:\curl folder.
+
+4. Go to http://curl.haxx.se/docs/caextract.html and download the digital certificate file named cacert.pem.
+
+The PEM file contains a bundle of valid digital certificates. The certificates are used to verify the authenticity of secure websites. They're distributed by certificate authority (CA) companies such as GlobalSign and VeriSign. The PEM file allows cURL to connect securely to the Zendesk API using the Secure Sockets Layer (SSL) protocol.
+
+5. Move the cacert.pem file to your C:\curl folder and rename it curl-ca-bundle.crt.
+
+6. Add the curl folder path to your Windows PATH environment variable so that the curl command is available from any location at the command prompt. Update the variable as follows:
+
+    1. In the Start menu, right-click This PC and select More > Properties. 
+  Note: In Windows 7, right-click Computer and select Properties.
+
+    2. Click Advanced System Settings.
+
+    3. In the Advanced tab, click the Environment Variables button on the lower right side.
+
+    4. Select the "Path" variable in System Variables, and click Edit.
+
+    5. In the Edit environment variable dialog box, click New and add the path to the curl.exe file. Example: C:\curl.
+
+# Executing script
+
+To start, we need to install the Dropbox Python SDK from the terminal:
+
+```
+pip install dropbox
+```
+or alternatively,
+```
+python3 -m pip install dropbox
+```
+
+Then to runt he script, simply run:
+```
+python3 zayne-THT-DropBox.py
+```
---
+++ zayne-THT-DropBox.py
@@ -0,0 +1,331 @@
+from datetime import datetime, timedelta
+import os
+import random
+import json
+import dropbox
+from dropbox import DropboxOAuth2FlowNoRedirect
+
+APP_KEY = "osaf9oqdzzd6wbx"
+APP_SECRET = "dr8hy4doxwj4586"
+APP_REFRESH_TOKEN = 'hWab75X6Qm8AAAAAAAAAATYN67Kptbi76QOInZ8cRqM7xMllwsv9VpfmQ2S4ESAA'
+ACCESS_TOKEN=''
+
+
+def print_logo():
+    print('')
+    print('██████╗░██████╗░░█████╗░██████╗░██████╗░░█████╗░██╗░░██╗')
+    print('██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗╚██╗██╔╝')
+    print('██║░░██║██████╔╝██║░░██║██████╔╝██████╦╝██║░░██║░╚███╔╝░')
+    print('██║░░██║██╔══██╗██║░░██║██╔═══╝░██╔══██╗██║░░██║░██╔██╗░')
+    print('██████╔╝██║░░██║╚█████╔╝██║░░░░░██████╦╝╚█████╔╝██╔╝╚██╗')
+    print('╚═════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░░░░╚═════╝░░╚════╝░╚═╝░░╚═╝')
+    print('')
+    print('░█████╗░░█████╗░███╗░░██╗███╗░░██╗███████╗░█████╗░████████╗░█████╗░██████╗░')
+    print('██╔══██╗██╔══██╗████╗░██║████╗░██║██╔════╝██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗')
+    print('██║░░╚═╝██║░░██║██╔██╗██║██╔██╗██║█████╗░░██║░░╚═╝░░░██║░░░██║░░██║██████╔╝')
+    print('██║░░██╗██║░░██║██║╚████║██║╚████║██╔══╝░░██║░░██╗░░░██║░░░██║░░██║██╔══██╗')
+    print('╚█████╔╝╚█████╔╝██║░╚███║██║░╚███║███████╗╚█████╔╝░░░██║░░░╚█████╔╝██║░░██║')
+    print('░╚════╝░░╚════╝░╚═╝░░╚══╝╚═╝░░╚══╝╚══════╝░╚════╝░░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝')
+    print('')
+
+def oauth_reg_flow():
+    """
+    Reg flow to get an access token online, as per Dropbox docs
+    We did some research and found a better way, with curl, which needs 
+    No user input to generate an access token
+    
+    Args:
+      
+    Returns:
+      
+    Raises:
+      none
+    """
+
+
+    auth_flow = DropboxOAuth2FlowNoRedirect(APP_KEY, APP_SECRET)
+
+    authorize_url = auth_flow.start()
+    print("1. Go to: " + authorize_url)
+    print("2. Click \"Allow\" (you might have to log in first).")
+    print("3. Copy the authorization code.")
+    auth_code = input("Enter the authorization code here: ").strip()
+
+    try:
+        oauth_result = auth_flow.finish(auth_code)
+    except Exception as e:
+        print('Error: %s' % (e,))
+        exit(1)
+
+    with dropbox.Dropbox(oauth2_access_token=oauth_result.access_token) as dbx:
+        dbx.users_get_current_account()
+        print("Successfully set up client!")
+    oauth_result = auth_flow.finish(auth_code)
+
+
+def get_cached_access_token():
+
+    """
+    get a cached (in local file storage) token
+    
+    Args:
+    
+    Returns:
+      the access token or False
+      
+    Raises:
+      none
+    """
+
+    file_exists = os.path.exists('access_cache.txt')
+    ACCESS_TOKEN = False
+
+    if not file_exists:
+        print('* Info: Access cache file does not exist, creating a new one')
+        ACCESS_TOKEN = get_new_access_token()
+    
+    # file should exist, containing access token
+    else:
+        f = open("access_cache.txt", "r")
+        last_updated_str = str(f.readline())
+        last_updated_str = last_updated_str.rstrip("\r\n")
+        last_updated_dt = datetime.strptime(last_updated_str, '%Y-%m-%d %H:%M:%S.%f')
+        ACCESS_TOKEN = f.readline()
+        f.close()
+    
+    # if file exists, and more than 3 hours have passed
+    if file_exists and(datetime.now() - last_updated_dt) > timedelta(hours=3):
+        print('* Info: Access cache file will expire soon, creating a new one')
+        ACCESS_TOKEN = get_new_access_token() # get new token as they expire after 4 hours
+
+    return ACCESS_TOKEN
+
+
+def get_new_access_token():
+    """
+    get a brand new access token if our cache is invalid
+    
+    Args:
+    
+    Returns:
+      Access tokem
+      
+    Raises:
+      none
+    """
+
+    # get short lived token via curl as per
+    # https://www.dropboxforum.com/t5/Dropbox-API-Support-Feedback/Get-refresh-token-from-access-token/m-p/596755/highlight/true#M27728
+    result = os.popen("curl -s https://api.dropbox.com/oauth2/token -d refresh_token="+APP_REFRESH_TOKEN+" -d grant_type=refresh_token -d client_id="+APP_KEY+" -d client_secret="+APP_SECRET+" ").read()
+    ACCESS_TOKEN = json.loads(result)['access_token']
+
+    f = open("access_cache.txt", "w")
+    f.write(str(datetime.now()))
+    f.write("\r\n")
+    f.write(ACCESS_TOKEN)
+    f.close()
+
+    return ACCESS_TOKEN
+
+
+def connect_dropbox(ACCESS_TOKEN):
+    """
+    Connect to dropbox api
+    
+    Args:
+      ACCESS_TOKEN: access token
+    
+    Returns:
+      drop box instance or or False
+      
+    Raises:
+      none
+    """
+    
+    dbx = False
+    
+    try:
+        dbx = dropbox.Dropbox(ACCESS_TOKEN)
+    except AuthError as e:
+        print('There was an error connecting to Dropbox ' + str(e))
+    return dbx
+
+
+def get_remote_file_list(dbx, path):
+
+    """
+    Get list of files in remote path specified
+    
+    Args:
+      dbx: drop box instance
+      path: remote path
+    
+    Returns:
+      True or nothing
+      
+    Raises:
+      none
+    """
+
+    # could also use: 
+    # for entry in dbx.files_list_folder('').entries:
+    #     print(entry.name)
+
+    files = dbx.files_list_folder(path).entries
+    f_list=[]
+
+    for file in files:
+        f_list.append({'name': file.name, 'modified': file.client_modified,})
+
+    return f_list
+
+
+def get_space_allocated(ACCESS_TOKEN):
+    """
+    return space allocated for user (2GB for free tier)
+    
+    Args:
+      ACCESS_TOKEN: access token
+    
+    Returns:
+      size alloc in mb
+      
+    Raises:
+      none
+    """
+
+    result = os.popen("curl -s -X POST https://api.dropboxapi.com/2/users/get_space_usage --header 'Authorization: Bearer "+ ACCESS_TOKEN +"'").read()
+    allocation = json.loads(result)['allocation']['allocated']
+    allocation = allocation / 1000
+    allocation = allocation / 1000
+    return int(allocation)
+
+
+def upload_file(local_file_path, dbx):
+    """
+    Upload local file to remote root dir
+    
+    Args:
+      local_file_path: local file + path str
+    
+    Returns:
+      
+    Raises:
+      none
+    """
+
+    remote_target_dir = '/'
+    file_name = os.path.basename(local_file_path)
+    remote_file_and_path = remote_target_dir + file_name
+
+    with open(local_file_path, 'rb') as f:
+        dbx.files_upload(f.read(), remote_file_and_path, mode=dropbox.files.WriteMode.overwrite)
+
+
+def generate_random_file():
+    """
+    generate a random file name, put some random text in it
+    
+    Args:
+      
+    Returns:
+      str path to new file
+      
+    Raises:
+      none
+    """
+    
+    fname = os.getcwd() + '/new-tmp-file-' + str(random.randint(1000,9999)) + '.txt'
+
+    f = open(fname, "w")
+    f.write(str(datetime.now()))
+    f.write("\r\n")
+    f.write('temp file')
+    f.close()
+
+    return str(fname)
+
+
+def main():
+    """
+    Dropbox connect
+    ---------------
+
+    print logo,
+    get an access token,
+    connect,
+    show remote user's name,
+    retrieve remote file list,
+    retrieve remote storage info,
+    create random file, insert some random text, and upload it,
+    retrieve updated remote file list
+
+
+    TODO:
+    enhancement: create readme
+    enhancement: we put it inside a class (define structure before coding)
+    enhancement: we put our settings in a seperate file that we import
+    enhancement: we show via a tree structure which shows dirs
+    enhancement: we show files inside tables on terminal
+    enhancement: we chnage our txt files to python code files with syntax highlighting
+    enhancement: MAke it constantly loop (like we did in prev task) and ask for input at bottom, 
+    """
+
+    print_logo()
+    
+    try:
+        ACCESS_TOKEN = get_cached_access_token()
+    except Exception:
+        abort(401)
+
+    dbx = connect_dropbox(ACCESS_TOKEN)
+    acc=dbx.users_get_current_account()
+
+    print("_________________________________________________________\r\n")
+    print('Good day, user: ' + acc.name.display_name)
+    print("_________________________________________________________")
+    print("\r\n\r\n")
+
+
+    print("Remote File list:________________________________________")
+    print("=========================================================\r\n")
+    flist = get_remote_file_list(dbx, '')
+    for file in flist:
+        print(file.get('name').rjust(3), file.get('modified'))
+        print('_________________________________________________________')
+    print("\r\n\r\n")
+    
+    print("Storage info:____________________________________________")
+    print("=========================================================\r\n")
+    space_usage = dbx.users_get_space_usage()
+    print('Used Space:      '+ str(space_usage.used) + ' Bytes')
+    print('Allocated Space: '+ str(get_space_allocated(ACCESS_TOKEN))+' MB')
+    print('_________________________________________________________')
+    print("\r\n\r\n")
+
+    print("File creation and upload:________________________________")
+    print("=========================================================\r\n")
+    new_random_file = generate_random_file()
+    print('New file created: '+ new_random_file)
+    print('Uploading new file: "'+os.path.basename(new_random_file)+'"" <<<<<<<')
+    upload_file(new_random_file, dbx)
+    print("\r\n\r\n")
+
+
+    print("Updated Remote File list:________________________________")
+    print("=========================================================\r\n")
+    flist = get_remote_file_list(dbx, '')
+    for file in flist:
+        print(file.get('name').ljust(3), file.get('modified'))
+        print('_________________________________________________________')
+    print("\r\n\r\n")
+    
+
+
+    
+
+
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
